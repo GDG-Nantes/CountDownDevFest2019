24ac8b83d6d56b29fb9165e9902f8a25922a2624
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2019-03-14 22:51:27 +0100

    add audio context

---
+++ audio.js
@@ -0,0 +1,65 @@
+'use strict'
+
+export class AudioPlayer {
+    constructor() {
+        try {
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            this.context = new AudioContext();
+        } catch (e) {
+            console.log("No WebAPI dectect");
+        }
+    }
+
+    _loadSound(url, bufferToUse) {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.responseType = 'arraybuffer';
+
+            // Decode asynchronously
+            request.onload = function () {
+                context.decodeAudioData(request.response, function (buffer) {
+                    this.buffer = buffer;
+                    resolve();
+                }, function (e) {
+                    reject();
+                    console.log('Error decoding file', e);
+                });
+            }
+            request.send();
+        })
+    }
+
+
+
+
+    /*****************************
+     ******************************
+     * Apis exposed
+     ******************************
+     ******************************
+     */
+
+    loadAndPlaySong(songPath) {
+        loadSound(songPath)
+            .then(_ => {
+                const source = this.context.createBufferSource(); // creates a sound source
+                source.buffer = this.buffer; // tell the source which sound to play
+                source.connect(this.context.destination); // connect the source to the context's destination (the speakers)
+                source.start(0); // play the source now
+                return source;
+            })
+            .then((source) => {
+                this.currentSource = source;
+            });
+    }
+
+
+    stop() {
+        if (this.currentSource && this.currentSource.stop) {
+            this.currentSource.stop(0);
+        }
+    }
+
+
+}
