62165769e55725bff5ae5ad0e246352cccb0f056
jefbinomed <jean.francois.garreau@gmail.com>
2019-10-04 16:53:10 +0200

    validate Config for deployement

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   // context: __dirname,
-  entry: "./src/index.js",
+  entry: './src/index.js',
   // target: 'node',
   output: {
     // path: path.resolve(__dirname, 'lib'),
-    filename: "./dist/bundle.js"
+    filename: './bundle.js',
   },
   // resolve: {
   //   extensions: ['.js']
   // },
-  devtool: "source-map"
-};
+  devtool: 'source-map',
+}
