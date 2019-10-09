f4e8114dd8bccac731323fcdc3338f138c046304
Jean-François Garreau - Binomed <jean.francois.garreau@gmail.com>
2019-10-09 23:59:07 +0200

    add cloudfunction for time sync

---
+++ functions/serverTime.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+var timesyncServer = require('timesync/server');
+const express = require('express');
+const cookieParser = require('cookie-parser')();
+const cors = require('cors');
+const app = express();
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(cookieParser);
+
+// handle timesync requests
+app.options('/whatTime', cors({
+    origin: '*'
+}))
+app.use('/whatTime', timesyncServer.requestHandler);
+
+
+exports.app = functions.https.onRequest(app);
