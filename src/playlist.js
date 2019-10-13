3603fec79516e10bc918ca67bd1b6777f22f047b
Benjamin Petetot <bpetetot@gmail.com>
2019-10-13 16:47:53 +0200

    revert playlist.js

--- src/playlist.js
+++ src/playlist.js
@@ -2,6 +2,319 @@
 
 export const PLAYLIST = [
   {
+    path: `killerqueen`,
+    name: 'Queen - Killer Queen',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `killinginthenameof`,
+    name: 'Rage against the machine - Killing in the name of',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `smellsliketeen`,
+    name: 'Nirvana - Smells like teen Spirit',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `The_Clash_-_Should_I_Stay_or_Should_I_Go`,
+    name: 'The Clash - Should I Stay or Should I Go (Live)',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `takemeout`,
+    name: 'Franz Ferdinand - Take me out',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Soundgarden_-_Black_Hole_Sun`,
+    name: 'Soundgarden - Black Hole Sun',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `messageinabottle`,
+    name: 'The Police - Message in a bottle',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `iloverockandroll`,
+    name: 'Joan Jett - I Love Rock And Roll',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `Evanescence_-_Bring_me_to_life`,
+    name: 'Evanescence - Bring me to life',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Blink-182_-_All_the_Small_Things`,
+    name: 'Blink 182 - All the small things',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `aroundtheworld`,
+    name: 'Red Hor Chilli Peppers - Around the world',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Aerosmith_-_Dream_On`,
+    name: 'Aerosmith - Dream On',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `7.4_Knights_of_Cydonia_-_ Muse`,
+    name: 'Muse - Knights of Cydonia',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `5.4_La_Grange_–_ZZ_Top`,
+    name: 'La Grange – ZZ Top',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `3.5_Paint_It,_Black_–_The_Rolling_Stones`,
+    name: 'Paint It, Black – The Rolling Stones',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `The_Police_-_Message_in_a_Bottle`,
+    name: 'Message in a Bottle - The Police',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `sweethomealabama`,
+    name: 'Lynyrd Skynyrd - Sweet home alabama',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Aerodynamic`,
+    name: 'Daft Punk - Aerodynamic',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'Song.ogg',
+      guitar: 'Guitar.ogg',
+    },
+  },
+  {
+    path: `A_Kind_Of_Magic`,
+    name: 'Queen - A Kind Of Magic',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'Song.ogg',
+      guitar: 'Guitar.ogg',
+    },
+  },
+  {
+    path: `giveitaway`,
+    name: 'Red Hor Chilli Peppers - Give it away',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `blackbetty`,
+    name: 'Ram Jam - Blackbetty',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Money_for_Nothing`,
+    name: 'Dire staits - Money for nothing',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Dire_Straits_-_Money_for_Nothing`,
+    name: 'Dire staits - Money for nothing',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `mich_-_beat`,
+    name: 'Michael Jackson - Beat it',
+    difficulty: 'MEDIUM_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `queen_-_bohemian`,
+    name: 'Queen - Bohemian Rapsody',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `starlight`,
+    name: 'Muse - Starlight',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Sunday_Bloody_Sunday`,
+    name: 'U2 - Sunday Bloody Sunday',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `U2_-_One`,
+    name: 'U2 - One',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `wearethechampions`,
+    name: 'Queen - We are the champions',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `wewillrockyou`,
+    name: 'Queen - We will rock you',
+    difficulty: 'AMAZING_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `5.5_Welcome_to_the_Jungle_–_Guns N_ Roses`,
+    name: 'Guns N Roses - Welcome to the jungle',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `4.5_Even_Flow_-_Pearl_Jam`,
+    name: 'Pearl Jam - Even Flow',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
+    path: `2._Evanescense_-_Sweet_Scent`,
+    name: 'Evanescense - Sweet Scent',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'guitar.ogg',
+    },
+  },
+  {
+    path: `Guns__N_Roses_-_Sweet_Child__O_Mine`,
+    name: "Guns 'N Roses - Sweet Child 'O Mine",
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+
+  {
+    path: `2.5_Bulls_on_Parade_–_Rage_Against_the_Machine`,
+    name: 'Bulls on Parade – Rage Against the Machine',
+    difficulty: 'EASY_DIFFICULTY',
+    song: {
+      song: 'song.ogg',
+      guitar: 'guitar.ogg',
+    },
+  },
+  {
     path: `4.1_Paranoid_–_Black_Sabbath`,
     name: 'Paranoid – Black Sabbath',
     difficulty: 'EASY_DIFFICULTY',
@@ -14,12 +327,11 @@ export const PLAYLIST = [
 
 export const LASTS_SONGS_PLAYLIST = [
   {
-    path: `4.1_Paranoid_–_Black_Sabbath`,
-    name: 'Paranoid – Black Sabbath',
+    path: `ACDC_-_Thunderstruck`,
+    name: 'ACDC - Thunderstruck',
     difficulty: 'EASY_DIFFICULTY',
     song: {
-      song: 'song.ogg',
-      guitar: 'guitar.ogg',
+      song: 'guitar.ogg',
     },
   },
 ]
